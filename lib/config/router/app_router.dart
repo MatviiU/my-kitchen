9728b88610062febe3875b07e5c12c57627b5b72
Matvii Ulytskyi <matveyulitskiy@gmail.com>
2025-08-02 18:26:00 +0200

    Added config

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,36 @@
+import 'package:go_router/go_router.dart';
+import 'package:my_kitchen/config/router/screen_names.dart';
+import 'package:my_kitchen/festures/products/presentation/views/add_product_screen.dart';
+import 'package:my_kitchen/festures/products/presentation/views/products_screen.dart';
+import 'package:my_kitchen/festures/recipes/presentation/views/recipe_screen.dart';
+import 'package:my_kitchen/festures/scanner/presentation/views/scanner_screen.dart';
+
+final appRouter = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      name: ScreenNames.productScreen,
+      builder: (context, state) => const ProductsScreen(),
+      routes: [
+        GoRoute(
+          path: 'add-product',
+          name: ScreenNames.addProductScreen,
+          builder: (context, state) => const AddProductScreen(),
+        ),
+        GoRoute(
+          path: 'scanner',
+          name: ScreenNames.scannerScreen,
+          builder: (context, state) => const ScannerScreen(),
+        ),
+        GoRoute(
+          path: 'recipes',
+          name: ScreenNames.recipesScreen,
+          builder: (context, state) {
+            final selectedIngredients = state.extra as List<String>?;
+            return RecipeScreen(selectedIngredients: selectedIngredients ?? []);
+          },
+        ),
+      ],
+    ),
+  ],
+);
---
+++ lib/config/router/screen_names.dart
@@ -0,0 +1,6 @@
+abstract class ScreenNames {
+  static const productScreen = 'products';
+  static const recipesScreen = 'recipes';
+  static const scannerScreen = 'scanner';
+  static const addProductScreen = 'add-product';
+}
